9656c9bcdc77775b542f48f2e8f6588f18768d7a
Keerti Kotaru <kvkirthy@gmail.com>
2022-02-11 17:23:25 +0530

    Minor enhancements

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
   const shareData = {
-      title: 'Drake ',
-      text: 'Share like a pro',
-      url: 'https://developer.mozilla.org'
+      title: 'Drake Doppelganger',
+      text: 'Share like a pro from your web application',
+      url: 'https://kvkirthy.github.io/web-share-sample'
     }
   
     const btn = document.querySelector('svg');
@@ -11,7 +11,7 @@
     btn.addEventListener('click', async () => {
       try {
         let result = await navigator.share(shareData);
-        document.getElementById("status").innerText = result;
+        document.getElementById("status").innerText = result || '';
       } catch(err) {
         document.getElementById("status").innerText = "Share not complete";
       }
